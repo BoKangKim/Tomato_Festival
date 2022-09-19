ad429915ec21e853b1fadf1464f1fa931b61ed86
YoungBean <dldudqls7733@naver.com>
2022-09-19 18:12:16 +0900

    Merge Shooting Game

---
+++ Assets/Script/CamEffect.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamEffect : MonoBehaviour
+{
+    float effectTime; // Time for the camera to shake time
+    float effecting; // for Time check
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectTime = 0.02f;
+        effecting = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine("CamEffectStart");
+        }
+    }
+
+    IEnumerator CamEffectStart()
+    {
+        while(effecting < effectTime)
+        {
+            effecting += Time.deltaTime;
+            float randomrotation_x = Random.Range(-0.7f, 0.7f);  // Random value for camera's rotation.x
+            float randomrotation_y = Random.Range(-3f, 3f); // Random value for camera's rotation.y
+
+            transform.rotation = Quaternion.Euler(randomrotation_x, randomrotation_y, 0); //camera's rotation.z value is fixed
+            yield return null;
+        }
+        effecting = 0f; //Reset
+        transform.rotation = Quaternion.Euler(0, 0, 0); //Reset
+    }
+}
---
+++ Assets/Script/GunAim.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunAim : MonoBehaviour
+{
+    Vector3 Mouse;
+    Vector2 dir;
+    float angle;
+    bool facingRight; //check the direction the gun is looking at
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Transforms a point from screen space into world space (mouse's screen position)
+        Mouse.z = 0f;
+        dir = (Mouse - transform.position).normalized; //direction from gun to mouse
+       
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        //this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); //Rotation angle degrees relative to z-axis
+
+        
+
+        if(transform.position.x <=  Mouse.x)
+        {
+            facingRight = true;
+        }
+        else
+        {
+            facingRight = false;
+        }
+            
+        
+        if (facingRight)
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, angle); //from euler to quaternion
+            // transform.rotation = Quaternion.LookRotation(Vector3.forward, Mouse - transform.position) * Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            //this.transform.rotation = Quaternion.Euler(180, 0, angle);
+            this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward) * Quaternion.AngleAxis(180, Vector3.right);
+        }
+
+
+        /*
+        var usingFlip = transform.position - target;
+
+        var scale = transform.localScale;
+        if (usingFlip.x > 0)
+        {
+            scale.x *= -1f;
+        }
+        transform.localScale = scale;
+        */
+    }
+}
